e5249b9fe5b84576dbe578fa401ad38962eeea41
voyagerok <shepard1010@gmail.com>
2017-02-12 15:18:36 +0300

    Initial Commit

---
+++ ios-image-filtering/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-image-filtering
+//
+//  Created by Николай on 12.02.17.
+//  Copyright © 2017 Николай. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
